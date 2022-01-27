0b43b101c3acfe99c3b207518a0edc663630d1cd
Akilah Snowden <akilahdsnowden@gmail.com>
2022-01-27 18:48:14 -0500

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name: "moya Dnicore",
+    streetAddress: "111 Bean Creek Road",
+};
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj};
+
+    newObj[key] = value;
+
+    return newObj;
+} 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj, key, value) {
+    return {
+        ...obj,
+        [key]: value,
+    }
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key]
+    return employee;
+}
